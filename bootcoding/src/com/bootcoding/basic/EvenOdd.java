30d6934da5a8d9f18e8b893f39b64226874adcb0
Prajakta33 <prajaktawankhede100@gmail.com>
2022-08-18 20:30:17 +0530

    Hello World Created

---
+++ bootcoding/src/com/bootcoding/basic/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.bootcoding.basic;
+
+public class EvenOdd {
+    public static void main(String ar[]){
+        int a=2;
+        int b=4;
+        if(b%a==0){
+            System.out.println("It is Even number");
+        }
+        else{
+            System.out.println("it is Odd number");
+        }
+    }
+}
