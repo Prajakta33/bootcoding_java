fb73a7670663231067b1308e3c2ee6ae52848a93
Prajakta33 <prajaktawankhede100@gmail.com>
2022-08-18 09:18:59 +0530

    Addition Created

---
+++ bootcoding/src/com/bootcoding/basic/addition.java
@@ -0,0 +1,11 @@
+package com.bootcoding.basic;
+
+public class addition{
+    public static void main(String ar[]){
+        int a=5 ;
+        int b=5 ;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}
