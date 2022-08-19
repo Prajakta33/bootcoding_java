a0a2fb553e67ffad9caffd6fbefb03cf3296c02f
Prajakta33 <prajaktawankhede100@gmail.com>
2022-08-19 08:06:47 +0530

    Person Education Created

---
+++ bootcoding/src/com/bootcoding/basic/PersonEducation.java
@@ -0,0 +1,24 @@
+package com.bootcoding.basic;
+
+public class PersonEducation {
+    public static void main(String[] args){
+        int a=20;
+    if(a<5){
+        System.out.println("Nursery\n");
+    }
+    if(a>5 && a<15){
+        System.out.println("School-K10\n");
+    }
+    if(a>16 && a<18){
+            System.out.println("High school-K12\n");
+        }
+    if(a>19 && a<22){
+        System.out.println("Graduate\n");
+    }
+        
+    }
+}
+
+
+
+
