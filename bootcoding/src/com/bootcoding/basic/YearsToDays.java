ba2a54921d5c0b56caf1037fcd77a6b693a6bc45
Prajakta33 <prajaktawankhede100@gmail.com>
2022-08-23 21:06:52 +0530

    Year to Days Conversion Created

---
+++ bootcoding/src/com/bootcoding/basic/YearsToDays.java
@@ -0,0 +1,12 @@
+package com.bootcoding.basic;
+
+public class YearsToDays {
+    public static void main(String [] args){
+       YearsToDays.age();
+    }
+    static void age(){
+        int day=365,age=20,c;
+        c=day*age;
+        System.out.println("Age in days is:\n"+c+" "+"days");
+    }
+}
