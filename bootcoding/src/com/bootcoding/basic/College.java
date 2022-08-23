e742f34b48f7bf55f2d556ed28cfe84a80fc6b0a
Prajakta33 <prajaktawankhede100@gmail.com>
2022-08-23 20:52:59 +0530

    College Grade Created

---
+++ bootcoding/src/com/bootcoding/basic/College.java
@@ -0,0 +1,20 @@
+package com.bootcoding.basic;
+
+public class College {
+    public static void main(String[] args){
+        String a="A";
+
+        if(a=="A+"){
+            System.out.println("ETC, CSE, MLE, AIE, ME, EE, CE");
+        }
+        if(a=="A"){
+            System.out.println("CSE,ETC, ME,EE,CE,MLE");
+        }
+        if(a=="B+"){
+            System.out.println("ETC, CSE, ME,EE");
+        }
+        if(a=="B"){
+            System.out.println("ETC,CSE,ME");
+        }
+    }
+}
