c0b3b24a710ac008cf245b6eb43c3380d6b2c9aa
Prajakta33 <prajaktawankhede100@gmail.com>
2022-08-23 21:07:41 +0530

    Foot to Centimeter Conversion Created

---
+++ bootcoding/src/com/bootcoding/basic/FootToCentimeter.java
@@ -0,0 +1,12 @@
+package com.bootcoding.basic;
+
+public class FootToCentimeter {
+    public static void main(String[] args){
+       FootToCentimeter.distance();
+    }
+    static void distance(){
+        double foot=30.48,cm=4,c;
+        c=foot*cm;
+        System.out.println("Foot in centimeter is:\n"+c+"cm");
+    }
+}
