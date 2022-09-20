4ae28ba5856613607deb67048dca0268d40cf4ff
Prajakta33 <prajaktawankhede100@gmail.com>
2022-09-20 20:43:01 +0530

    Program for star Pattern is created

---
+++ bootcoding/src/com/bootcoding/star/StarApplication.java
@@ -0,0 +1,49 @@
+package com.bootcoding.star;
+
+import java.util.Date;
+
+public class StarApplication {
+
+    public static void main(String[] args) {
+        int n = 4;
+        //System.out.println("*\n");
+        //System.out.println("* *\n");
+        //System.out.println("* * *\n");
+
+       /* for(int i=0; i<n;i++){
+
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+        /*for(int i=0; i<=n;i++){
+
+            for(int j=0;j<=n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+        Date st=new Date();
+
+        Date et=new Date();
+        System.out.println(et.getTime()-st.getTime()+"ms");
+
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            for(int p=0;p<i*2;p++) {
+                System.out.print(" ");
+            }
+            for (int a = 0; a <= n-i; a++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
