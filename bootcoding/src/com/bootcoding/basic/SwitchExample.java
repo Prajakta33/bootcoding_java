7557719ba16f62f6f592f5a758737182ebdbf5b3
Prajakta33 <prajaktawankhede100@gmail.com>
2022-09-20 20:27:00 +0530

    switch example is created

---
+++ bootcoding/src/com/bootcoding/basic/SwitchExample.java
@@ -0,0 +1,34 @@
+package com.bootcoding.basic;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        int week = 6;
+
+        switch (week){
+            case 1:
+                System.out.println("Week 1");
+                break;
+            case 2:
+                System.out.println("Week 2");
+                break;
+            case 3:
+                System.out.println("Week 3");
+                break;
+            case 4:
+                System.out.println("Week 4");
+                break;
+            case 5:
+                System.out.println("Week 5");
+                break;
+            case 6:
+                System.out.println("Week 6");
+                break;
+            case 7:
+                System.out.println("Week 7");
+                break;
+            default:
+                System.out.println("Default");
+
+        }
+    }
+}
