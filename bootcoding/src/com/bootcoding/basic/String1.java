39db0ff6fe8f52a4f3c38a4f073e96a0103b70f5
Prajakta33 <prajaktawankhede100@gmail.com>
2022-09-20 20:27:36 +0530

    string example is created

---
+++ bootcoding/src/com/bootcoding/basic/String1.java
@@ -0,0 +1,14 @@
+package com.bootcoding.basic;
+
+public class String1 {
+
+    public static void main(String[] args) {
+
+
+        String str1 = "  Hello  ";
+        String str2 = str1.trim();
+
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
