239bd81292775b6dedd5f856bae955deea7be6f4
Prajakta33 <prajaktawankhede100@gmail.com>
2022-09-20 20:40:39 +0530

    Practice(Solution) Program is created

---
+++ bootcoding/src/com/bootcoding/dsa/Solution.java
@@ -0,0 +1,23 @@
+package com.bootcoding.dsa;
+
+public class Solution {
+
+    public static void main(String[] args){
+
+        int n=15;
+        for(int i=1;i<=n;i++) {
+            if (i%5==0 && i%3==0) {
+                System.out.println("FizzBuzz");
+            }else
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+
+            } else {
+                System.out.println(" " +i);
+            }
+        }
+    }
+}
